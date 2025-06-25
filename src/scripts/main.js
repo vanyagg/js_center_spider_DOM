a7c670280337d26e6f6403807172d87af2760e42
Vanya Holovey <golovejvana9@gmail.com>
2025-06-25 21:17:57 +0300

    fixed everything

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,5 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
-spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
+spider.style.top = `${(wall.clientHeight - spider.clientHeight) / 2}px`;
+spider.style.left = `${(wall.clientWidth - spider.clientWidth) / 2}px`;
